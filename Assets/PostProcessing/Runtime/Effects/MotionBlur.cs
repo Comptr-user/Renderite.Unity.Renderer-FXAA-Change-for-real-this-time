763b883ada5c28519ac3a66e92d6cbe85f44e1bb
Comptr-user <tf2player6@gmail.com>
2026-06-10 13:30:41 -0500

    Re-Add changes that allows motion blur in cameras when in VR
    While the overall post processing update did change the exact phrasing to "!context.stereoActive" instead of 
    "!RuntimeUtilities.isVREnabled" it's probably safe to assume that they function similarly enough to warrant keeping that check commented out.
    
    Signed-off-by: Comptr-user <tf2player6@gmail.com>

--- Assets/PostProcessing/Runtime/Effects/MotionBlur.cs
+++ Assets/PostProcessing/Runtime/Effects/MotionBlur.cs
@@ -38,7 +38,7 @@ namespace UnityEngine.Rendering.PostProcessing
             #endif
                 && SystemInfo.supportsMotionVectors
                 && RenderTextureFormat.RGHalf.IsSupported()
-                && !context.stereoActive;
+                /*&& !context.stereoActive*/;
         }
     }
 
